1771c9dd016d5ae571c57266537890f9d212990f
LabCap <banlazthosow@gmail.com>
2022-08-24 14:23:41 +0300

    v2

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 function createTask() {
-  let taskText = document.querySelector(".todo__input").value;
+  const taskText = document.querySelector(".todo__input").value;
+  console.log(taskText);
+  const todoInfo = document.querySelector(".todo__info");
 
-  document.querySelector(".todo__bottom").insertAdjacentHTML(
-    "afterend",
-    `
+  if (taskText == "") {
+    todoInfo.classList.add("error");
+    todoInfo.innerHTML = `
+    Error, you cannot add an empty entry
+`;
+  } else {
+    todoInfo.classList.remove("error");
+    todoInfo.innerHTML = `
+
+`;
+    document.querySelector(".todo__bottom").insertAdjacentHTML(
+      "afterend",
+      `
   <div class="todo__task">
     <button class="todo__btn todo__btn-done btn-green btn" >Done</button> 
     <p class="todo__text">${taskText}</p>
     <button class="todo__btn todo__btn-remove btn-red btn">Remove</button>
   </div>
   `
-  );
+    );
+  }
 
   const todoText = document.querySelectorAll(".todo__text");
   // console.log(todoText);
@@ -26,9 +39,9 @@ function createTask() {
     });
   });
 
-  let todoTask = document.querySelectorAll(".todo__task");
+  const todoTask = document.querySelectorAll(".todo__task");
   // console.log(todoTask);
-  let todoBtnRemove = document.querySelectorAll(".todo__btn-remove");
+  const todoBtnRemove = document.querySelectorAll(".todo__btn-remove");
   // console.log(todoBtnRemove);
 
   todoBtnRemove.forEach((item, i) => {
